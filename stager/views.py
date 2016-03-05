32d55a0538497482a563e1877a4f007a9f56fffc
Ivo Lima <ivo.romario@gmail.com>
2016-03-05 04:07:40 +0100

    testing webhook 1

--- stager/views.py
+++ stager/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods, require_POST
 
 def index(request):
-    return HttpResponse("Pagina inicial")
+    return HttpResponse("Pagina inicial!")
 
 @require_POST
 def add_repository(request):
