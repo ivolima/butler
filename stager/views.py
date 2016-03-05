09ee0e1d032431ec999909e907bcc0f29a2cc76e
Ivo Lima <ivo.romario@gmail.com>
2016-03-05 20:45:15 +0100

    first draft of webhooks view

--- stager/views.py
+++ stager/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods, require_POST
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
-def index(request):
+def dashboard(request):
     return HttpResponse("Pagina inicial!")
 
-@csrf_exempt
 @require_POST
 def add_repository(request):
     import ipdb; ipdb.set_trace()
     print request
 
+@csrf_exempt
+@require_POST
+def webhooks(request, user_id, project_id):
+    import ipdb; ipdb.set_trace()
+    print request
 
