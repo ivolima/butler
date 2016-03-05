63fa70df30324142eefea4ea30d6671cde102b28
Ivo Lima <ivo.romario@gmail.com>
2016-03-05 04:02:44 +0100

    bugfix

--- stager/views.py
+++ stager/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.http import require_http_methods, require_POST
 def index(request):
     return HttpResponse("Pagina inicial")
 
-@require_POST()
+@require_POST
 def add_repository(request):
     import ipdb; ipdb.set_trace()
     print request
