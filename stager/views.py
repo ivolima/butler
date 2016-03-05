c782d633fb51b983d950e04b77a73cda75def721
Ivo Lima <ivo.romario@gmail.com>
2016-03-05 04:18:57 +0100

    added csrf_exempt decorator to webhook view

--- stager/views.py
+++ stager/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods, require_POST
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 def index(request):
     return HttpResponse("Pagina inicial!")
 
+@csrf_exempt
 @require_POST
 def add_repository(request):
     import ipdb; ipdb.set_trace()
