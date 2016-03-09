5d61fa24c4752c7433f36c6ee32de7e73034ccf2
Ivo Lima <ivo.romario@gmail.com>
2016-03-10 00:40:08 +0100

    removed model Profile and updated Project

--- stager/models.py
+++ stager/models.py
@@ -5,26 +5,27 @@ from django.contrib.auth.models import User
 import uuid
 
 
-class Profile(models.Model):
-    # Unique identifier fieldi
-    cod_uuid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False, verbose_name=(u'UUID'))
-    user = models.OneToOneField(User, unique=True, verbose_name=('user'), related_name='my_profile')
+#class Profile(models.Model):
+    # Unique identifier field
+#    cod_uuid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False, verbose_name=(u'UUID'))
+#    user = models.OneToOneField(User, unique=True, verbose_name=('user'), related_name='my_profile')
 
-    class Meta:
-        verbose_name = (u'Profile')
-        verbose_name_plural = (u'Profiles')
+#    class Meta:
+#        verbose_name = (u'Profile')
+#        verbose_name_plural = (u'Profiles')
         #ordering = ('user_last_name',)
 
-    def __unicode_(self):
-        return unicode(self.user.username)
+#    def __unicode_(self):
+#        return unicode(self.user.username)
 
-    def __str__(self):
-        return str(self.user.username)
+#    def __str__(self):
+#        return str(self.user.username)
 
 class Project(models.Model):
     cod_uuid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False, verbose_name=(u'UUID'))
     repository_url = models.URLField(default="")
-    profile = models.ForeignKey(Profile)
+    name = models.CharField(max_length=40, default="")
+    owner = models.ForeignKey(User)
     is_private = models.BooleanField(default=None)
     scm = models.CharField(max_length=10)
     language = models.CharField(max_length=40, default="")
